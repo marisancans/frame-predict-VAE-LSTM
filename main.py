eb2c1550c49458bad4bd894ebc65257dd1988b32
maris <marisancans@gmail.com>
2020-03-14 11:31:19 +0200

    Working example

--- main.py
+++ main.py
@@ -1,138 +1,7 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
-import numpy as np
-
-class Encoder(nn.Module):
-
-    def __init__(self, input_size, hidden_dim, num_layers=1):
-        super(Encoder, self).__init__()
-
-        self.input_size = input_size
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(self.input_size, self.hidden_dim, num_layers=self.num_layers)
-        self.hidden = None
-
-    def init_hidden(self, batch_size):
-        return (torch.zeros(self.num_layers, batch_size, self.hidden_dim),
-                torch.zeros(self.num_layers, batch_size, self.hidden_dim))
-
-    def forward(self, inputs):
-        # Push through RNN layer (the ouput is irrelevant)
-        _, self.hidden = self.lstm(inputs, self.hidden)
-        return self.hidden
-
-
-class Decoder(nn.Module):
-
-    def __init__(self, hidden_dim, num_layers=1):
-        super(Decoder, self).__init__()
-        # input_size=1 since the output are single values
-        self.lstm = nn.LSTM(1, hidden_dim, num_layers=num_layers)
-        self.out = nn.Linear(hidden_dim, 1)
-
-    def forward(self, outputs, hidden, criterion):
-        batch_size, num_steps = outputs.shape
-        # Create initial start value/token
-        input = torch.tensor([[0.0]] * batch_size, dtype=torch.float)
-        # Convert (batch_size, output_size) to (seq_len, batch_size, output_size)
-        input = input.unsqueeze(0)
-
-        loss = 0
-        for i in range(num_steps):
-            # Push current input through LSTM: (seq_len=1, batch_size, input_size=1)
-            output, hidden = self.lstm(input, hidden)
-            # Push the output of last step through linear layer; returns (batch_size, 1)
-            output = self.out(output[-1])
-            # Generate input for next step by adding seq_len dimension (see above)
-            input = output.unsqueeze(0)
-            # Compute loss between predicted value and true value
-            loss += criterion(output, outputs[:, i])
-        return loss
-
-
-if __name__ == '__main__':
-
-    # 5 is the number of features of your data points
-    encoder = Encoder(2, 3)
-    decoder = Decoder(3)
-    # Create optimizers for encoder and decoder
-    encoder_optimizer = optim.Adam(encoder.parameters(), lr=0.001)
-    decoder_optimizer = optim.Adam(decoder.parameters(), lr=0.001)
-    criterion = nn.MSELoss()
-
-    # Some toy data: 2 sequences of length 10 with 2 features for each data point
-    inputs = [
-        [
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-        ],
-        [
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-            [0.5, 0.2],
-        ]
-    ]
-
-    inputs = torch.tensor(np.array(inputs), dtype=torch.float)
-    # Convert (batch_size, seq_len, input_size) to (seq_len, batch_size, input_size)
-    inputs = inputs.transpose(1,0)
-
-    # 2 sequences (to match the batch size) of length 6 (for the 6h into the future)
-    outputs = [ [0.1, 0.2, 0.3, 0.1, 0.2, 0.3], [0.3, 0.2, 0.1, 0.3, 0.2, 0.1] ]
-    outputs = torch.tensor(np.array(outputs), dtype=torch.float)
-
-    #
-    # Do one complete forward & backward pass
-    #
-    # Zero gradients of both optimizers
-    encoder_optimizer.zero_grad()
-    decoder_optimizer.zero_grad()
-    # Reset hidden state of encoder for current batch
-    encoder.hidden = encoder.init_hidden(inputs.shape[1])
-    # Do forward pass through encoder
-    hidden = encoder(inputs)
-    # Do forward pass through decoder (decoder gets hidden state from encoder)
-    loss = decoder(outputs, hidden, criterion)
-    # Backpropagation
-    loss.backward()
-    # Update parameters
-    encoder_optimizer.step()
-    decoder_optimizer.step()
-    print("Loss:", loss.item())
-
-
-
-
-
-
-
-
-
-
-
-
 import cv2, torch, os, datetime
 import numpy as np
 from torch import nn
-
+import torchvision
 import multiprocessing
 from os import getpid
 import argparse
@@ -167,15 +36,30 @@ parser.add_argument('-r', type=float, default=5, help='Radius of generated image
 parser.add_argument('-load', default='', help='Specify save folder name and the last epoch will be tanken')
 args = parser.parse_args()
 
-def show(truth_t, pred_t):
-    img_t = torch.cat((truth_t, pred_t), dim=2)
-    img_t = img_t.permute(1, 2, 0)
+def show(truth_t, pred_t, steps, imgs_dir, epoch):
+    B, S, H, W = truth_t.shape
+    longest = max(steps)
+
+    f = 0
+    imgs = []
+
+    for s in np.cumsum(steps):        
+        for t, p in zip(truth_t[f: s], pred_t[f:s]):
+            imgs.append(t)
+            imgs.append(p)
+
+        pad = (longest - (s - f)) * 2
+        for n in range(pad):
+            imgs.append(torch.zeros(1, H, W))
+        f = s
+
+    grid_t = torchvision.utils.make_grid(imgs, nrow=longest * 2)
    
-    img = img_t.cpu().detach().numpy()
+    img = grid_t.cpu().permute(1, 2, 0).detach().numpy()
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.imshow('image', img)
     cv2.waitKey(1)
-   
+    cv2.imwrite(imgs_dir + '/' + str(epoch) + '.png', img)
 
 def load(path):
     if not os.path.exists(path):
@@ -206,11 +90,14 @@ def collate_fn(data):
     imgs, idxs = pack(data, 'imgs')
     imgs_truths, idxs_truths = pack(data, 'imgs_truths')
 
+    # gather how far we have to predict
+    steps = [len(x['imgs_truths']) for x in data]
+
     # 2D sequences to 1D array
     batch_1D = torch.cat(imgs)
     batch_truths_1D = torch.cat(imgs_truths)
 
-    return batch_1D, idxs, batch_truths_1D, idxs_truths
+    return batch_1D, idxs, batch_truths_1D, idxs_truths, steps
 
 
 print('Using device:', args.device)
@@ -221,6 +108,7 @@ model = ModifiedUNet(args, in_channels=1, out_channels=1, bottleneck_out=None, i
 now = datetime.datetime.now()
 dir_name = now.strftime("%B_%d_at_%H_%M_%p")
 save_dir = './save/' + dir_name
+imgs_dir = './imgs/' + dir_name
 
 optimizer = torch.optim.Adam(model.parameters(), args.lr)
 reconstruction_loss_fn = torch.nn.BCELoss()
@@ -232,22 +120,18 @@ for epoch in range(1, args.epoch):
     epoch_loss_rec = []
     epoch_loss_kl = []
 
-    for batch, idxs, batch_truths, idxs_truths in dataset_loader:
+    for batch, idxs, batch_truths, idxs_truths, steps in dataset_loader:
         batch = batch.to(args.device)
+        batch_truths = batch_truths.to(args.device)
         
-        preds = model.forward(batch, idxs)
-
+        decoded, z_mu, z_sigma = model.forward(batch, idxs, steps)
         
-
-        decoded = decoder.forward(lstm_last_stacked)
-
         if args.debug:
-            show(last_frames_t[0], decoded[0])
-        
-        if args.load:
-            continue
+            if not os.path.exists(imgs_dir):
+                os.makedirs(imgs_dir)
+            show(batch_truths.detach().cpu(), decoded.detach().cpu(), steps, imgs_dir, epoch)
 
-        loss_recunstruction = reconstruction_loss_fn(decoded, last_frames_t)
+        loss_recunstruction = reconstruction_loss_fn(decoded, batch_truths)
 
         loss_kl = args.beta * 0.5 * (1.0 + torch.log(z_sigma**2) - z_mu**2 - z_sigma**2)
         loss_kl = torch.mean(loss_kl)
@@ -272,9 +156,7 @@ for epoch in range(1, args.epoch):
 
         torch.save({
                 'epoch': epoch,
-                'encoder': encoder.state_dict(),
-                'decoder': encoder.state_dict(),
-                'lstm': lstm.state_dict(),
+                'encoder': model.state_dict(),
                 'optimizer': optimizer.state_dict(),
                 'loss': loss,
             }, save_dir + f'/{epoch}_{float(loss)}_save.pth')
--- models.py
+++ models.py
@@ -4,9 +4,19 @@ import torchvision.models as models
 import torch.nn as nn
 from collections import OrderedDict
 
-class LSTMModel(nn.Module):
+def conv_output_shape(h_w, kernel_size=(1, 1), stride=(1, 1), pad=(0, 0), dilation=1):   
+    h = (h_w[0] + (2 * pad[0]) - (dilation * (kernel_size[0] - 1)) - 1)// stride[0] + 1
+    w = (h_w[1] + (2 * pad[1]) - (dilation * (kernel_size[1] - 1)) - 1)// stride[1] + 1
+    return h, w
+
+def convtransp_output_shape(h_w, kernel_size=(1, 1), stride=(1, 1), pad=(0, 0), dilation=1):       
+    h = (h_w[0] - 1) * stride[0] - 2 * pad[0] + kernel_size[0] + pad[0]
+    w = (h_w[1] - 1) * stride[1] - 2 * pad[1] + kernel_size[1] + pad[1]
+    return h, w
+
+class LSTMEncoder(nn.Module):
     def __init__(self, input_size, hidden_size, device='cpu', num_layers=1):
-        super(LSTMModel, self).__init__()
+        super(LSTMEncoder, self).__init__()
 
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -26,9 +36,23 @@ class LSTMModel(nn.Module):
 
         return hidden_last_timestep
 
+class LSTMDecoder(nn.Module):
+    def __init__(self, input_size, hidden_size, device='cpu', num_layers=1):
+        super(LSTMDecoder, self).__init__()
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=False)
+        self.device = device
+
+    def forward(self, sizes, steps, hidden):
+        S, B, z = sizes
+        input = torch.zeros([max(steps), B, z], dtype=torch.float).to(self.device)
+        preds, hidden = self.lstm(input, hidden)
+
+        preds = preds.permute(1, 0, 2) # (Seq_len, B, z) --> (B, Seq_len, z)
+        return preds
+
 # https://github.com/mateuszbuda/brain-segmentation-pytorch
 class ModifiedUNet(nn.Module):
-
     def __init__(self, args, in_channels=1, out_channels=1, bottleneck_out=None, init_features=32):
         super(ModifiedUNet, self).__init__()
 
@@ -44,12 +68,16 @@ class ModifiedUNet(nn.Module):
         # encoder
         self.encoder1 = ModifiedUNet._block(in_channels, features, name="enc1")
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.bn_enc1 = nn.BatchNorm2d(features)
         self.encoder2 = ModifiedUNet._block(features, features * 2, name="enc2")
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.bn_enc2 = nn.BatchNorm2d(features * 2)
         self.encoder3 = ModifiedUNet._block(features * 2, features * 4, name="enc3")
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.bn_enc3 = nn.BatchNorm2d(features * 4)
         self.encoder4 = ModifiedUNet._block(features * 4, features * 8, name="enc4")
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.bn_enc4 = nn.BatchNorm2d(features * 8)
 
         # bottleneck
         self.bottleneck = ModifiedUNet._block(features * 8, self.bottleneck_out, name="bottleneck")
@@ -60,51 +88,83 @@ class ModifiedUNet(nn.Module):
         self.sigma = torch.nn.Linear(in_features=self.bottleneck_out, out_features=self.args.z_size)
 
         # LSTM
-        self.lstm = LSTMModel(input_size=args.z_size, hidden_size=args.z_size, device=args.device).to(args.device)
+        self.lstm_encoder = LSTMEncoder(input_size=args.z_size, hidden_size=args.z_size, device=args.device).to(args.device)
+        self.lstm_decoder = LSTMDecoder(input_size=args.z_size, hidden_size=args.z_size, device=args.device).to(args.device)  
 
         # decoder
+        
+        # calculate what shape should be before upconv4
+        self.upconv6 = nn.ConvTranspose2d(
+            args.z_size, features * 16, kernel_size=2, stride=2
+        )
+        self.decoder6 = ModifiedUNet._block(features * 16, features * 16, name="dec6")
+        self.bn_dec6 = nn.BatchNorm2d(features * 16)
+
+        self.upconv5 = nn.ConvTranspose2d(
+            features * 16, features * 16, kernel_size=2, stride=2
+        )
+        self.decoder5 = ModifiedUNet._block(features * 16, features * 16, name="dec5")
+        self.bn_dec5 = nn.BatchNorm2d(features * 16)
+
+
         self.upconv4 = nn.ConvTranspose2d(
             features * 16, features * 8, kernel_size=2, stride=2
         )
-        self.decoder4 = ModifiedUNet._block((features * 8) * 2, features * 8, name="dec4")
+        self.decoder4 = ModifiedUNet._block(features * 8, features * 8, name="dec4")
+        self.bn_dec4 = nn.BatchNorm2d(features * 8)
+
+
         self.upconv3 = nn.ConvTranspose2d(
             features * 8, features * 4, kernel_size=2, stride=2
         )
-        self.decoder3 = ModifiedUNet._block((features * 4) * 2, features * 4, name="dec3")
+        self.decoder3 = ModifiedUNet._block(features * 4, features * 4, name="dec3")
+        self.bn_dec3 = nn.BatchNorm2d(features * 4)
+
         self.upconv2 = nn.ConvTranspose2d(
             features * 4, features * 2, kernel_size=2, stride=2
         )
-        self.decoder2 = ModifiedUNet._block((features * 2) * 2, features * 2, name="dec2")
+        self.decoder2 = ModifiedUNet._block(features * 2, features * 2, name="dec2")
+        self.bn_dec2 = nn.BatchNorm2d(features * 2)
+
         self.upconv1 = nn.ConvTranspose2d(
             features * 2, features, kernel_size=2, stride=2
         )
-        self.decoder1 = ModifiedUNet._block(features * 2, features, name="dec1")
+        self.decoder1 = ModifiedUNet._block(features, features, name="dec1")
+        self.bn_dec1 = nn.BatchNorm2d(features)
 
         self.conv = nn.Conv2d(
             in_channels=features, out_channels=out_channels, kernel_size=1
         )
 
-    def lstm_predict(self, z_sequences):
-        lstm_out = self.lstm.forward(z_sequences)
 
-        # take 
-        lstm_last = [seq[idx] for seq, idx in zip(unpacked, unpacked_len-1)]
-        lstm_last_stacked = torch.stack(lstm_last)
-        x=0
 
-    def forward(self, x, idxs):
+    def forward(self, x, idxs, steps):
         enc1 = self.encoder1(x)
-        enc2 = self.encoder2(self.pool1(enc1))
-        enc3 = self.encoder3(self.pool2(enc2))
-        enc4 = self.encoder4(self.pool3(enc3))
+        enc1 = self.bn_enc1(enc1)
+        enc1 = F.relu(enc1)
+
+        enc2 = self.pool1(enc1)
+        enc2 = self.encoder2(enc2)
+        enc2 = self.bn_enc2(enc2)
+        enc2 = F.relu(enc2)
+
+        enc3 = self.pool2(enc2)
+        enc3 = self.encoder3(enc3)
+        enc3 = self.bn_enc3(enc3)
+        enc3 = F.relu(enc3)
+
+        enc4 = self.pool3(enc3)
+        enc4 = self.encoder4(enc4)
+        enc4 = self.bn_enc4(enc4)
+        enc4 = F.relu(enc4)
 
         bottleneck = self.bottleneck(self.pool4(enc4))
-        z_vector = self.avg_pool(bottleneck)
-        z_vector = z_vector.squeeze(-1).squeeze(-1)
+        avg_pooled = self.avg_pool(bottleneck)
+        avg_pooled = avg_pooled.squeeze(-1).squeeze(-1)
                 
         # VAE
-        z_mu = self.mu.forward(z_vector)
-        z_sigma = self.sigma.forward(z_vector)
+        z_mu = self.mu.forward(avg_pooled)
+        z_sigma = self.sigma.forward(avg_pooled)
 
         bs = x.shape[0]
         eps = torch.randn(bs, self.args.z_size).to(self.args.device) * z_sigma + z_mu # Sampling epsilon from normal distributions
@@ -117,24 +177,55 @@ class ModifiedUNet(nn.Module):
         z_sequences = torch.stack(z_sequences)
         z_sequences = z_sequences.permute(1, 0, 2) # (B, Seq_len, z_vector) --> (Seq_len, B, z_vector)
  
-        # get next N frames from LSTM
-        next_pred = self.lstm_predict(z_sequences)
+        # encode time, return last lastm hidden state
+        hidden_last_timestep = self.lstm_encoder.forward(z_sequences)
 
+        # decode and predict time 
+        preds = self.lstm_decoder.forward(z_sequences.shape, steps, hidden_last_timestep)
         
+        # pick predictions that we need
+        picked_preds = []
+        for batch_idx, to_step in enumerate(steps):
+            picked_preds.append(preds[batch_idx][0:to_step])
 
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
+        masked_catted = torch.cat(picked_preds, dim=0)           
+
+        # add H and W dims
+        masked_catted = masked_catted.unsqueeze(-1).unsqueeze(-1)
+
+        expand = self.upconv6(masked_catted)
+        expand = self.decoder6(expand)
+        expand = self.bn_dec6(expand)
+        expand = F.relu(expand)
+        expand = self.upconv5(expand)
+        expand = self.decoder5(expand)
+        expand = self.bn_dec5(expand)
+        expand = F.relu(expand)
+
+        dec4 = self.upconv4(expand)
         dec4 = self.decoder4(dec4)
+        dec4 = self.bn_dec4(dec4)
+        dec4 = F.relu(dec4)
+
         dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
         dec3 = self.decoder3(dec3)
+        dec3 = self.bn_dec3(dec3)
+        dec3 = F.relu(dec3)
+
         dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
         dec2 = self.decoder2(dec2)
+        dec2 = self.bn_dec2(dec2)
+        dec2 = F.relu(dec2)
+
         dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
         dec1 = self.decoder1(dec1)
-        return torch.sigmoid(self.conv(dec1))
+        dec1 = self.bn_dec1(dec1)
+        dec1 = F.relu(dec1)
+
+        out = self.conv(dec1)
+        out = torch.sigmoid(out)
+        return out, z_mu, z_sigma
+
 
     @staticmethod
     def _block(in_channels, features, name):
